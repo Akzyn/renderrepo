e9acc7e34bf0b3478ff74a2fb347ff978c72d069
Nicte <167065469+Akzyn@users.noreply.github.com>
2024-10-16 01:50:27 -0600

    Version 2

--- app.py
+++ app.py
@@ -36,7 +36,21 @@ if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
     st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
-    st.session_state['df_experiment_results'] = pd.concat((st.session_state['df_experiment_results'], pd.DataFrame(data=[[st.session_state['experiment_no'], number_of_trials, mean]], columns=['no', 'iterations', 'mean'])), axis=0) st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
     
+    # Concatenar los resultados del experimento
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]], 
+                     columns=['no', 'iterations', 'mean'])
+    ], axis=0)
+    
+    # Resetear el índice
+    st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
+
+# Mostrar los resultados
 st.write(st.session_state['df_experiment_results'])
+
+
 st.write('Esta aplicación aún no es funcional. En construcción.')
