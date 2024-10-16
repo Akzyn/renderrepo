686fde6e657c509e5ea4ffc4868568436cda5ab0
Nicte <167065469+Akzyn@users.noreply.github.com>
2024-10-15 23:54:22 -0600

    Actualizacion de app.py con nuevos cambios scipy y time

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
+import scipy.stats
 import streamlit as st
+import time
 
 st.title('Lanzar una moneda')
+
+chart = st.line_chart([0.5])
+
+def toss_coin(n): #funcion que emula el lanzamiento de una moneda
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+    for r in trial_outcomes:
+        outcome_no +=1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+        
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
 start_button = st.button('Ejecutar')
 if start_button:
